b689491dcb0c106576588dfd717f611a91517d43
Matt Farmer <mrfarmer777@gmail.com>
2018-07-19 02:23:33 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,8 +1,5 @@
 class Artist < ActiveRecord::Base
   has_many :songs
 
-  def self.artist_select
-    select_tag "song[artist_id]", options_for_select(Artist.all.collect{|a| [a.name, a.id]})
-    #collection_select(:song, :artist_id, Artist.all, :id, :name))
-  end
+  
 end
